d16ac135d994717a1f1167cdb2487266c091bf89
Bryan <bryandesigning@gmail.com>
2021-08-15 14:42:14 -0400

    Add FxDk Convar Support

--- packages/ghmattimysql/src/static/fxmanifest.lua
+++ packages/ghmattimysql/src/static/fxmanifest.lua
@@ -25,3 +25,19 @@ files {
 }
 
 ui_page 'ui/index.html'
+
+convar_category 'MySQL' {
+    "GHMattiMySQL Configuration Options",
+    {
+        { "Connection String", "mysql_connection_string", "CV_STRING", "" },
+        { "Debug Mode", "mysql_debug", "CV_MULTI", {
+            { "None", "None" },
+            { "Console", "Console" },
+            { "File", "File"},
+            { "FileAndConsole", "FileAndConsole" }
+        }},
+        { "Slow Query Warning", "mysql_slow_query_warning", "CV_COMBI", 100, 0, 5000 },
+        { "Log Level", "mysql_log_level", "CV_INT", 15, 1, 15 },
+        { "Log File Format", "mysql_log_file_format", "CV_STRING", "%s-%d.log" },
+    }
+}
